4ce0c640a0551ad14b4d0cd69f1273234e599137
david02-bowie <davidalejandromerinoespinosa@gmail.com>
2024-10-18 18:32:05 -0500

    primer commit

---
+++ miprimerproyecto01/src/ejercicio1/Clase1.java
@@ -0,0 +1,9 @@
+package ejercicio1;
+
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("ejemplo basico de java versionado a traves de Git manejado desde GitHub");
+    }
+    
+}
