3e1a9d7d32b945cca480cd6b53998b4b884713ae
david02-bowie <davidalejandromerinoespinosa@gmail.com>
2024-10-19 09:21:47 -0500

    segundo commit

--- miprimerproyecto01/src/ejercicio1/Clase1.java
+++ miprimerproyecto01/src/ejercicio1/Clase1.java
@@ -3,7 +3,7 @@ package ejercicio1;
 public class Clase1 {
 
     public static void main(String[] args) {
-        System.out.println("ejemplo basico de java versionado a traves de Git manejado desde GitHub");
+        System.out.println("Ejemplo basico de java versionado a traves de Git manejado desde GitHub");
     }
     
 }
