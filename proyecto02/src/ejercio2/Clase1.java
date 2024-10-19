68d33199ad2e5fae84915fe4ac2af32b40d0a52f
david02-bowie <davidalejandromerinoespinosa@gmail.com>
2024-10-19 10:08:37 -0500

    tercer commit, trabajando con Git y GitHubclassroom

---
+++ proyecto02/src/ejercio2/Clase1.java
@@ -0,0 +1,10 @@
+package ejercio2;
+
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("ejemplo 2 basico de java versionado a traves de Git manejado a traves desde mi GitHub classroom");
+        
+    }
+    
+}
