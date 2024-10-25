91e9d5634e8580cb02984459637c9859b5fc31c3
david02-bowie <davidalejandromerinoespinosa@gmail.com>
2024-10-25 16:49:38 -0500

    Intento 01

--- proyecto02/src/ejercio2/Clase1.java
+++ proyecto02/src/ejercio2/Clase1.java
@@ -4,7 +4,7 @@ public class Clase1 {
 
     public static void main(String[] args) {
         System.out.println("ejemplo 2 basico de java versionado a traves de Git manejado a traves desde mi GitHub classroom");
-        
+        System.out.println("hola");
     }
     
 }
