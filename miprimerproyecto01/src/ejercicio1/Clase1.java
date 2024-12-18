d38c012c091a94a7e3448de9813a59b287ac4820
JuanDa0304 <jdvargas17@utpl.edu.ec>
2024-12-18 00:09:03 -0500

    arreglo

--- miprimerproyecto01/src/ejercicio1/Clase1.java
+++
@@ -1,9 +0,0 @@
-package ejercicio1;
-
-public class Clase1 {
-
-    public static void main(String[] args) {
-        System.out.println("Ejemplo basico de java versionado a traves de Git manejado desde GitHub");
-    }
-    
-}
